06dbb28512d8a4c21a7f6ca8e0bdef937398351a
braynen305-byte <braynen305@gmail.com>
2026-05-24 19:13:33 -0400

    >

--- dbnotes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    middlename VARCHAR(30) NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- includes/dbnotes.sql
+++ includes/dbnotes.sql
@@ -1,9 +1,13 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    middlename VARCHAR(30) NULL,
-    lastname VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    middle_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
+    gender VARCHAR(10) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    telephone VARCHAR(20) NOT NULL,
+    residence VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
